e1b4ce479b258662a32c5ce75a5857180a1dd02f
reagan-meant <reaganmeant@gmail.com>
2021-06-03 12:30:55 +0300

    Add MSSP report(Vaccinations)

--- api/src/main/java/org/openmrs/module/commonreports/CommonReportsConstants.java
+++ api/src/main/java/org/openmrs/module/commonreports/CommonReportsConstants.java
@@ -35,6 +35,9 @@ public class CommonReportsConstants {
 	
 	public static final String COMPONENT_REPORTMANAGER_LAB = MODULE_ARTIFACT_ID + ".lab";
 	
+	public static final String COMPONENT_REPORTMANAGER_VACCINATION = MODULE_ARTIFACT_ID + ".vaccination";
+	
+	public static final String COMPONENT_REPORTMANAGER_ECV = MODULE_ARTIFACT_ID + ".ecv";
 	
 	/*
 	 * URIs URLs
