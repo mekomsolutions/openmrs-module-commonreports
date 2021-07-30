3c650d34b3ea923decc7f1d1423131dead7bae11
rbuisson <romain@mekomsolutions.com>
2021-07-30 14:31:18 +0200

    C2C-90: Formatting

--- api/src/main/java/org/openmrs/module/commonreports/reports/PatientHistoryReportManager.java
+++ api/src/main/java/org/openmrs/module/commonreports/reports/PatientHistoryReportManager.java
@@ -49,118 +49,118 @@ import org.springframework.stereotype.Component;
 
 @Component(CommonReportsConstants.COMPONENT_REPORTMANAGER_PATIENTHISTORY)
 public class PatientHistoryReportManager extends ActivatedReportManager {
-
+	
 	public final static String REPORT_DESIGN_UUID = "563d9679-ffc1-45e8-b4fd-b1988eaf1895";
-
+	
 	public final static String REPORT_DEFINITION_NAME = "Patient History";
-
+	
 	public final static String DATASET_KEY_DEMOGRAPHICS = "demographics";
-
+	
 	public final static String DATASET_KEY_OBS = "obs";
-
+	
 	public final static String DATASET_KEY_ENCOUNTERS = "encounters";
-
+	
 	public final static String VISIT_UUID_LABEL = "visit_uuid";
-
+	
 	public final static String VISIT_LOCATION_LABEL = "visit_location";
-
+	
 	public final static String VISIT_TYPE_LABEL = "visit_type";
-
+	
 	public final static String ENCOUNTER_UUID_LABEL = "encounter_uuid";
-
+	
 	public final static String ENCOUNTER_PROVIDER_LABEL = "provider_name";
-
+	
 	public final static String ENCOUNTER_TYPE_UUID_LABEL = "encounter_type_uuid";
-
+	
 	public final static String ENCOUNTERTYPE_NAME_LABEL = "encounter_type_name";
-
+	
 	public final static String ENCOUNTER_DATETIME_LABEL = "encounter_datetime";
-
+	
 	public final static String OBS_VALUE_LABEL = "obs_value";
-
+	
 	public final static String OBS_DATETIME_LABEL = "obs_datetime";
-
+	
 	public final static String OBS_DATATYPE_LABEL = "concept_datatype";
-
+	
 	public final static String OBS_NAME_LABEL = "concept_name";
-
+	
 	public final static String OBS_PROVIDER_LABEL = "provider_name";
-
+	
 	public final static String OBS_ID_LABEL = "obs_id";
-
+	
 	public final static String OBS_GROUP_ID_LABEL = "obs_group_id";
-
+	
 	@Autowired
 	private EncounterDataLibrary encounterDataLibrary;
-
+	
 	@Autowired
 	private BuiltInPatientDataLibrary builtInPatientDataLibrary;
-
+	
 	@Autowired
 	private ObsDataLibrary obsDataLibrary;
-
+	
 	@Autowired
 	BasePatientDataLibrary basePatientDataLibrary;
-
+	
 	@Override
 	public boolean isActivated() {
 		return super.isActivated();
 	}
-
+	
 	@Override
 	public String getVersion() {
 		return "1.1.0-SNAPSHOT";
 	}
-
+	
 	@Override
 	public String getUuid() {
 		return "ae2152c1-1a91-4dc9-96d2-dfc38820648e";
 	}
-
+	
 	@Override
 	public String getName() {
 		return "HC1 Patient History";
 	}
-
+	
 	@Override
 	public String getDescription() {
 		return StringUtils.EMPTY;
 	}
-
+	
 	private Parameter getLocationParameter() {
 		return new Parameter("locationList", "Visit Location", Location.class, List.class, null);
 	}
-
+	
 	@Override
 	public List<Parameter> getParameters() {
 		List<Parameter> params = new ArrayList<Parameter>();
 		params.add(getLocationParameter());
 		return params;
 	}
-
+	
 	@Override
 	public ReportDefinition constructReportDefinition() {
 		ReportDefinition reportDef = new ReportDefinition();
 		reportDef.setUuid(this.getUuid());
 		reportDef.setName(REPORT_DEFINITION_NAME);
 		reportDef.setDescription(this.getDescription());
-
+		
 		Map<String, Object> mappings = new HashMap<String, Object>();
 		MessageSourceService i18nTranslator = Context.getMessageSourceService();
-
+		
 		// Create dataset definitions
 		PatientDataSetDefinition patientDataSetDef = createDemographicsDataSetDefinition(i18nTranslator);
 		ObsDataSetDefinition obsDataSetDef = createObsAndEncounterDataSetDefinition();
 		EncounterDataSetDefinition encountersDatasetSetDef = createEncounterAndVisitDataSetDefinition();
-
+		
 		// Add datasets to the report
 		reportDef.addDataSetDefinition(DATASET_KEY_DEMOGRAPHICS, patientDataSetDef, mappings);
 		reportDef.addDataSetDefinition(DATASET_KEY_OBS, obsDataSetDef, mappings);
 		reportDef.addDataSetDefinition(DATASET_KEY_ENCOUNTERS, encountersDatasetSetDef, new HashMap<String, Object>());
-
+		
 		return reportDef;
 	}
-
+	
 	private EncounterDataSetDefinition createEncounterAndVisitDataSetDefinition() {
 		EncounterDataSetDefinition encounterAndVistDatasetSetDef = new EncounterDataSetDefinition();
 		encounterAndVistDatasetSetDef.addColumn(VISIT_UUID_LABEL, encounterDataLibrary.getVisitId(), StringUtils.EMPTY,
@@ -182,7 +182,7 @@ public class PatientHistoryReportManager extends ActivatedReportManager {
 		encounterAndVistDatasetSetDef.addSortCriteria(ENCOUNTER_DATETIME_LABEL, SortCriteria.SortDirection.DESC);
 		return encounterAndVistDatasetSetDef;
 	}
-
+	
 	/**
 	 * @param i18nTranslator
 	 * @return
@@ -203,10 +203,10 @@ public class PatientHistoryReportManager extends ActivatedReportManager {
 		    builtInPatientDataLibrary.getGender());
 		addColumn(patientDataSetDef, i18nTranslator.getMessage("commonreports.patienthistory.demographics.fulladdress"),
 		    basePatientDataLibrary.getAddressFull());
-
+		
 		return patientDataSetDef;
 	}
-
+	
 	/**
 	 * @return
 	 */
@@ -229,7 +229,7 @@ public class PatientHistoryReportManager extends ActivatedReportManager {
 		obsDataSetDef.addSortCriteria(OBS_DATETIME_LABEL, SortCriteria.SortDirection.DESC);
 		return obsDataSetDef;
 	}
-
+	
 	@Override
 	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
 		ReportDesign reportDesign = new ReportDesign();
@@ -239,7 +239,7 @@ public class PatientHistoryReportManager extends ActivatedReportManager {
 		reportDesign.setRendererType(PatientHistoryXmlReportRenderer.class);
 		return Arrays.asList(reportDesign);
 	}
-
+	
 	private void addColumn(PatientDataSetDefinition dsd, String columnName, PatientDataDefinition pdd) {
 		dsd.addColumn(columnName, pdd, Mapped.straightThroughMappings(pdd));
 	}
